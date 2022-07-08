7fe9aee778525b1633b6623d9e844ad72d70046a
Lulz07 <masterluffy2007@gmail.com>
2022-07-08 20:29:15 +0800

    Add my own PowerShell profile

---
+++ poshprofile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,35 @@
+# shell config
+$PSDefaultParameterValues["*:Encoding"] = "utf8"
+
+# remove unnecessary aliases (cause i want to use msys64 one)
+del alias:man -Force
+del alias:ls -Force
+del alias:wget -Force
+del alias:curl -Force
+del alias:cp -Force
+del alias:chdir -Force
+del alias:clear -Force
+del alias:diff -Force
+del alias:cat -Force
+del alias:mv -Force
+del alias:pwd -Force
+del alias:rm -Force
+del alias:rmdir -Force
+del alias:sort -Force
+del alias:tee -Force
+
+# my own aliases
+Function PacmanUpdate {pacman -Syuu}
+New-Alias -Name update -Value PacmanUpdate
+
+Function PacmanInstalled {pacman -Q}
+New-Alias -Name installed -Value PacmanInstalled
+
+Function VimPROFILE {vim $PROFILE}
+New-Alias -Name prf -Value VimPROFILE
+
+Function VimVIMRC {vim ~/_vimrc}
+New-Alias -Name vrc -Value VimVIMRC
+
+# banner
+bash neofetch --gap 10
